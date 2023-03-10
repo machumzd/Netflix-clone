37dd3eb4865ac279a3d2318fc70260cdff963d46
machumzd <www.machuzz123@gmail.com>
2023-03-10 21:15:28 +0530

    iframe added,hover of buttons and images

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,7 +4,7 @@ function NavBar() {
   return (
     <div className="navbar" >
     <img className="logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png" alt="Netflix Logo"/>
-    <img className="avatar" src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png" alt="Avatar"/>
+    <img className="avatar" src="https://i.postimg.cc/cHSj3MZ2/92350947.jpg" alt="Avatar"/>
 </div>
   )
 }
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import "./Posts.css"
-import {imageUrl} from '../../constants/Constants'
+import {API_KEY, imageUrl} from '../../constants/Constants'
 import { useEffect,useState } from 'react'
 import axios from '../../axios'
+import YouTube  from 'react-youtube'
 function Posts(props) {
     const [movies,setMovies]=useState([])
+   const [urlId,setUrlId]=useState('')
     useEffect(()=>{
         axios.get(props.url).then((response)=>{
             setMovies(response.data.results)
@@ -12,14 +14,33 @@ function Posts(props) {
             // alert(err);
         })
     })
+      const opts = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  }
+  const handleMovie=(id)=>{
+  console.log(id)
+  axios.get(`/movie/${id}/videos?api_key=${API_KEY}&language=en-US`).then(response=>{
+    if(response.data.results.length!==0){
+      setUrlId(response.data.results[0])
+    }else{
+      console.log("url not found or array empty")
+    }
+      })
+  }
   return (
     <div className='row'>
     <h2>{props.title}</h2>
     <div className='posters'>
         {movies.map((obj)=>
-       <img className={props.isSmall ?'smallPoster':'poster' }alt='poster' src={`${imageUrl+obj.backdrop_path}`} />
+       <img  onClick={()=>handleMovie(obj.id)} className={props.isSmall ?'smallPoster':'poster' }alt='poster' src={`${imageUrl+obj.backdrop_path}`} />
 )}
     </div>
+   {urlId && <YouTube opts={opts} videoId={urlId.key}></YouTube>}
     </div>
   )
 }
